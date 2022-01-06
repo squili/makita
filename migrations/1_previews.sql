fbed8b11e722d0fdc9c1bc74d0f2edf3a3f39542
squili <74628488+squili@users.noreply.github.com>
2022-01-05 19:52:06 -0800

    update licenses

--- migrations/1_previews.sql
+++ migrations/1_previews.sql
@@ -1,4 +1,4 @@
--- Copyright 2021 Squili
+-- Copyright 2021 Mia Stoaks
 -- This program is distributed under the terms of the GNU Affero General Public License
 -- You should have received a copy of the license along with this program
 -- If not, see <https://www.gnu.org/licenses/#AGPL>
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,8 @@
+// Copyright 2021 Mia Stoaks
+// This program is distributed under the terms of the GNU Affero General Public License
+// You should have received a copy of the license along with this program
+// If not, see <https://www.gnu.org/licenses/#AGPL>
+
 pub use crate::api::utils::ApiContext;
 pub use crate::error::BotError;
 pub use crate::macros::{debug, s};
